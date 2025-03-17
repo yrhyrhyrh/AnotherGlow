35ea35536fe944f43d9d832d93172270a56f4b02
RyanCodesHard <ryan.taw96@gmail.com>
2025-03-17 16:39:12 +0800

    added routing

--- frontend/appFrontend/src/app/app.component.ts
+++ frontend/appFrontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RandomComponent } from './random/random.component';
 
 @Component({
   selector: 'app-root',
---
+++ frontend/appFrontend/src/app/random/random.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-random',
+  imports: [],
+  templateUrl: './random.component.html',
+  styleUrl: './random.component.css'
+})
+export class RandomComponent {
+  
+}
