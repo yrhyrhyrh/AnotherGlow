4570f3b23f80b75389604ff1d25c72f76fa62736
RyanCodesHard <ryan.taw96@gmail.com>
2025-03-26 16:45:03 +0800

    merged changes from main

--- backend/appBackend/Program.cs
+++ backend/appBackend/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 using MyAppBackend.Repositories;
 using MyAppBackend.Services;
 using System.Data.Common;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +13,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
 builder.Services.AddScoped<IWeatherForecastRepository, WeatherForecastRepository>();
 
+var key = "my_secret_key_12345"; // 🔥 Change this to a secure key!
+
 // Add services to the container
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+        };
+    });
+builder.Services.AddAuthorization();
 builder.Services.AddControllers(); // Make sure controllers are registered here
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(); // Swagger generation setup
@@ -36,7 +54,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseHttpsRedirection();
 app.MapControllers();
 
