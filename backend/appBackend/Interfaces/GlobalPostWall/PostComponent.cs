b5d63442f9e0a358ac0a66f208ef82a0e4ab31af
yrhyrhyrh <yu.runhan12@gmail.com>
2025-05-02 02:20:13 +0800

    Use composite pattern for posts

---
+++ backend/appBackend/Interfaces/GlobalPostWall/PostComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace appBackend.Interfaces.GlobalPostWall
+{
+    public abstract class PostComponent
+    {
+        public abstract string Type { get; }
+
+        public virtual void Add(PostComponent component) { throw new NotImplementedException(); }
+
+    }
+
+}
---
+++ backend/appBackend/Models/PostComponents/AttachmentComponent.cs
@@ -0,0 +1,12 @@
+﻿using appBackend.Interfaces.GlobalPostWall;
+
+namespace appBackend.Models.PostChildrenComponents
+{
+    public class AttachmentComponent : PostComponent
+    {
+        public Attachment Attachment { get; set; }
+
+        public override string Type => "Attachment";
+        public AttachmentComponent(Attachment attachment) { Attachment = attachment; }
+    }
+}
