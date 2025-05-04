b46fb0f52140426ee20ce479a48bf52915c43799
yrhyrhyrh <yu.runhan12@gmail.com>
2025-05-05 01:46:20 +0800

    Composite design pattern

--- backend/appBackend/Models/PostComponents/AttachmentComponent.cs
+++ backend/appBackend/Models/PostComponents/AttachmentComponent.cs
@@ -1,12 +1,29 @@
-﻿using appBackend.Interfaces.GlobalPostWall;
+﻿using appBackend.Dtos.GlobalPostWall;
+using appBackend.Interfaces.GlobalPostWall;
 
 namespace appBackend.Models.PostChildrenComponents
 {
     public class AttachmentComponent : PostComponent
     {
-        public Attachment Attachment { get; set; }
+        private Attachment _attachment;
+
+        public AttachmentComponent(Attachment attachment)
+        {
+            _attachment = attachment;
+        }
 
         public override string Type => "Attachment";
-        public AttachmentComponent(Attachment attachment) { Attachment = attachment; }
+
+        public override object ToDTO(Guid currentUserId)
+        {
+            return new AttachmentDTO
+            {
+                AttachmentId = _attachment.AttachmentId,
+                FileName = _attachment.FileName,
+                FilePath = _attachment.FilePath,
+                ContentType = _attachment.ContentType,
+                FileSize = _attachment.FileSize
+            };
+        }
     }
 }
